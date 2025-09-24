8c6db545d1aaed4561ccdb719bd24009251d5aa9
shogo takata <bsi25019@uhe.ac.jp>
2025-09-24 14:11:55 +0900

    init

---
+++ src/Exp6_2.java
@@ -0,0 +1,19 @@
+
+public class Exp6_2 {
+
+	public static void main(String[] args) {
+
+		int n = 13;
+		int step = 0;
+		
+	    while (n != 1) { 
+            step++; // 1 回処理をするごとにカウント  
+            if (n % 2 == 0) { 
+                    n = n / 2;                //  偶数なら 2 で割る 
+            } else { 
+                    n = 3 * n + 1;        //  奇数なら 3 倍して 1 を足す 
+            } 
+    }
+	}
+
+}
