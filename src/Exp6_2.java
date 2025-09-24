eeedcb18d6b9a943e5f4db553233976c3ad78fe9
shogo takata <bsi25019@uhe.ac.jp>
2025-09-24 14:20:29 +0900

    step表示機能の追加

--- src/Exp6_2.java
+++ src/Exp6_2.java
@@ -3,17 +3,19 @@ public class Exp6_2 {
 
 	public static void main(String[] args) {
 
-		int n = 13;
-		int step = 0;
-		
-	    while (n != 1) { 
-            step++; // 1 回処理をするごとにカウント  
-            if (n % 2 == 0) { 
-                    n = n / 2;                //  偶数なら 2 で割る 
-            } else { 
-                    n = 3 * n + 1;        //  奇数なら 3 倍して 1 を足す 
-            } 
-    }
+			int n = 13;
+			int step = 0;
+			
+		    while (n != 1) { 
+	            step++;  
+	            if (n % 2 == 0) { 
+	                    n = n / 2;                
+	            } else { 
+	                    n = 3 * n + 1;        
+	            } 
+	    }
+		    
+		   System.out.println("1 になるまでに” + step +”回の操作をしました");
 	}
 
 }
